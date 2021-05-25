a23913a64f67a97b6f7d3e698cb61d2b68e5f5ca
Cordovani <lor.cordovani@gmail.com>
2021-05-25 10:39:55 +0200

    scaffolding

---
+++ script.js
@@ -0,0 +1,17 @@
+const logisticsApp = new Vue({
+    el:"#mainApp",
+    data: {
+        // 
+    },
+    // array di oggetti per i dati dei link nella navbar
+    navLink:[
+        {}
+    ],
+    // array di oggetti per i dati dei link nell footer
+    footLink:[{
+
+    }
+    ]
+}
+
+)
