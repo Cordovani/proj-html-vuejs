01f600a6f1cc6412d0b2b0a913ebfb4c5fa99cdb
Cordovani <lor.cordovani@gmail.com>
2021-05-26 16:42:25 +0200

    header e hero

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const logisticsApp = new Vue({
-    el:"#mainApp",
+    el: "#mainApp",
     data: {
-        // 
-    },
-    // array di oggetti per i dati dei link nella navbar
-    navLink:[
-        {}
-    ],
-    // array di oggetti per i dati dei link nell footer
-    footLink:[{
 
+        // array di oggetti per i dati dei link nella navbar
+        navLink: [
+            {
+                title: "HOME",
+                link: "#"
+            },
+            {
+                title: "SERVICES",
+                link: "#"
+            },
+            {
+                title: "SKILLS",
+                link: "#"
+            },
+            {
+                title: "PARTNERS",
+                link: "#"
+            },
+            {
+                title: "BLOG",
+                link: "#"
+            },
+        ],
+        // array di oggetti per i dati dei link nell footer
+        footLink: [{
+
+        }
+        ]
     }
-    ]
 }
 
 )
